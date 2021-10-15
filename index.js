dc860e6b4278b6d1857b40566442bd5afc058e05
BTchorzewski <tchorzewskibartosz@gmail.com>
2021-10-15 16:00:57 +0200

    connect api routes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const todoRouter = require('./router/todo');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.static('public'));
 
-
+app.use('/api', todoRouter);
 
 app.get('/', (req, res) => {
   res.status(200).send('index.html');
